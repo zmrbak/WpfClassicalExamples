29ac8bc005bb8b1ea16e284cc0eda796eddd6ced
zmrbak <20995658@qq.com>
2019-07-01 11:05:51 +0800

    L009：如何捕捉WPF应用程序中未处理的异常

---
+++ L009/L009/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace L009
+{
+    /// <summary>
+    /// App.xaml 的交互逻辑
+    /// </summary>
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            DispatcherUnhandledException += App_DispatcherUnhandledException;
+        }
+
+        private void App_DispatcherUnhandledException(object sender, System.Windows.Threading.DispatcherUnhandledExceptionEventArgs e)
+        {
+            //throw new NotImplementedException();
+            MessageBox.Show("DispatcherUnhandledException，异常已处理！");
+            e.Handled = true;
+        }
+    }
+}
---
+++ L009/L009/MainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace L009
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            AppDomain.CurrentDomain.UnhandledException += CurrentDomain_UnhandledException;
+        }
+
+        private void CurrentDomain_UnhandledException(object sender, UnhandledExceptionEventArgs e)
+        {
+            //throw new NotImplementedException();
+            MessageBox.Show("CurrentDomain_UnhandledException，异常已处理！");
+            //e.Handled = true;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (rd_0.IsChecked == true)
+            {
+                try
+                {
+                    throw new Exception("引发异常 ");
+                }
+                catch (Exception)
+                {
+                    MessageBox.Show("捕捉到异常");
+                }
+            }
+
+            if (rd_1.IsChecked == true)
+            {
+                throw new Exception("引发异常 ");
+            }
+        }
+    }
+}
