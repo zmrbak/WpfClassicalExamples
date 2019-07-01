52369a114894c551b99ad10af2e1ab1aa92109f6
zmrbak <20995658@qq.com>
2019-07-01 11:04:43 +0800

    L008：如何向WPF应用程序传递启动参数

---
+++ L008/L008/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace L008
+{
+    /// <summary>
+    /// App.xaml 的交互逻辑
+    /// </summary>
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            var arg = e.Args;
+            if(arg.Contains("/window1"))
+            {
+                new Window1().Show();
+            }
+            else
+            {
+                new MainWindow().Show();
+            }
+
+        }
+
+    }
+}
