b40b1e6af2f91e391789c87df665d00fcf2fead2
zmrbak <20995658@qq.com>
2019-07-01 11:02:44 +0800

    L006：如何在窗口(Window)之间建立所属关系

---
+++ L006/L006/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace L006
+{
+    /// <summary>
+    /// App.xaml 的交互逻辑
+    /// </summary>
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            MainWindow window = new MainWindow();
+            window.Show();
+
+            ToolBox toolBox = new ToolBox();
+            toolBox.Owner = window;
+            toolBox.Show();
+
+        }
+    }
+}
