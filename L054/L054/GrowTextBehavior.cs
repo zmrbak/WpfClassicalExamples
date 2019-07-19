aada36d7e7db51c2d386026bca534e21f3201b2f
zmrbak <20995658@qq.com>
2019-07-20 06:59:50 +0800

    L054：如何使用行为来扩展控件的功能

---
+++ L054/L054/GrowTextBehavior.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Interactivity;
+
+namespace L054
+{
+    public class GrowTextBehavior : Behavior<TextBlock>
+    {
+        public int GrowBySize { set; get; }
+
+        protected override void OnAttached()
+        {
+            base.OnAttached();
+            AssociatedObject.MouseEnter += AssociatedObject_MouseEnter;
+            AssociatedObject.MouseLeave += AssociatedObject_MouseLeave;
+        }
+
+        private void AssociatedObject_MouseLeave(object sender, System.Windows.Input.MouseEventArgs e)
+        {
+            //throw new NotImplementedException();
+            AssociatedObject.FontSize -= GrowBySize;
+        }
+
+        private void AssociatedObject_MouseEnter(object sender, System.Windows.Input.MouseEventArgs e)
+        {
+            //throw new NotImplementedException();
+            AssociatedObject.FontSize += GrowBySize;
+        }
+
+        protected override void OnDetaching()
+        {
+            base.OnDetaching();
+            AssociatedObject.MouseEnter -= AssociatedObject_MouseEnter;
+            AssociatedObject.MouseLeave -= AssociatedObject_MouseLeave;
+        }
+    }
+    public class GrowTextBehavior1 : Behavior<Label>
+    {
+        public int GrowBySize { set; get; }
+
+        protected override void OnAttached()
+        {
+            base.OnAttached();
+            AssociatedObject.MouseEnter += AssociatedObject_MouseEnter;
+            AssociatedObject.MouseLeave += AssociatedObject_MouseLeave;
+        }
+
+        private void AssociatedObject_MouseLeave(object sender, System.Windows.Input.MouseEventArgs e)
+        {
+            //throw new NotImplementedException();
+            AssociatedObject.FontSize -= GrowBySize;
+        }
+
+        private void AssociatedObject_MouseEnter(object sender, System.Windows.Input.MouseEventArgs e)
+        {
+            //throw new NotImplementedException();
+            AssociatedObject.FontSize += GrowBySize;
+        }
+
+        protected override void OnDetaching()
+        {
+            base.OnDetaching();
+            AssociatedObject.MouseEnter -= AssociatedObject_MouseEnter;
+            AssociatedObject.MouseLeave -= AssociatedObject_MouseLeave;
+        }
+    }
+}
