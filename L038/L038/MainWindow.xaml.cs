113fdde111da18b85385439e1f102e684e056b41
zmrbak <20995658@qq.com>
2019-07-20 06:48:22 +0800

    L038：如何使用CLR属性以及UI通知

---
+++ L038/L038/MainWindow.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace L038
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window,INotifyPropertyChanged
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        public String Department
+        {
+            get { return "软件工程部"; }
+        }
+
+        public string PersonName
+        {
+            get { return personName; }
+            set
+            {
+                personName = value;
+                OnPropertyChanged("PersonName");
+            }
+        }
+
+        private String personName;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged(String propertyName)
+        {
+            PropertyChanged?.Invoke(this,new PropertyChangedEventArgs(propertyName));
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            //提交
+            MessageBox.Show("hi," + PersonName);
+
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            //重置
+            PersonName = "";
+        }
+    }
+}
