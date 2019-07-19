0eb81cd134da24a5b97f5ac3fbd1e41e53eb61c6
zmrbak <20995658@qq.com>
2019-07-20 06:35:04 +0800

    L022：如何在WPF应用程序中使用列表框控件

---
+++ L022/L022/MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace L022
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        int index = 10;
+        private void Bt_Add_Click(object sender, RoutedEventArgs e)
+        {
+            index++;
+            lb.Items.Add(new ListBoxItem() {  Content="选项:"+ index });
+        }
+
+        private void Bt_Delete_Click(object sender, RoutedEventArgs e)
+        {
+            List<Object> itmes = new List<object>();
+            foreach (var item in lb.SelectedItems)
+            {
+                itmes.Add(item);
+                
+            }
+
+            foreach (var item in itmes)
+            {
+                lb.Items.Remove(item);
+            }
+
+        }
+    }
+}
