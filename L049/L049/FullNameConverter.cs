a6cbe86cd3e2bb38f2f6417ca071522658e6591e
zmrbak <20995658@qq.com>
2019-07-20 06:56:04 +0800

    L049：如何在WPF中使用多值转换器

---
+++ L049/L049/FullNameConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace L049
+{
+    public class FullNameConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            //throw new NotImplementedException();
+            return values[0]+" "+ values[1] + " "+values[2];
+
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            //throw new NotImplementedException();
+            return value.ToString().Split(' ');
+        }
+    }
+}
