70cf5f687762a3fc726e625f4da624d82b25a177
zmrbak <20995658@qq.com>
2019-07-20 06:59:08 +0800

    L053：如何在自定义控件中公开事件

---
+++ L053/L053/SearchControl.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace L053
+{
+    /// <summary>
+    /// 按照步骤 1a 或 1b 操作，然后执行步骤 2 以在 XAML 文件中使用此自定义控件。
+    ///
+    /// 步骤 1a) 在当前项目中存在的 XAML 文件中使用该自定义控件。
+    /// 将此 XmlNamespace 特性添加到要使用该特性的标记文件的根
+    /// 元素中:
+    ///
+    ///     xmlns:MyNamespace="clr-namespace:L053"
+    ///
+    ///
+    /// 步骤 1b) 在其他项目中存在的 XAML 文件中使用该自定义控件。
+    /// 将此 XmlNamespace 特性添加到要使用该特性的标记文件的根
+    /// 元素中:
+    ///
+    ///     xmlns:MyNamespace="clr-namespace:L053;assembly=L053"
+    ///
+    /// 您还需要添加一个从 XAML 文件所在的项目到此项目的项目引用，
+    /// 并重新生成以避免编译错误:
+    ///
+    ///     在解决方案资源管理器中右击目标项目，然后依次单击
+    ///     “添加引用”->“项目”->[浏览查找并选择此项目]
+    ///
+    ///
+    /// 步骤 2)
+    /// 继续操作并在 XAML 文件中使用控件。
+    ///
+    ///     <MyNamespace:SearchControl/>
+    ///
+    /// </summary>
+    public class SearchControl : Control
+    {
+        static SearchControl()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(SearchControl), new FrameworkPropertyMetadata(typeof(SearchControl)));
+        }
+
+        public string SearchTerm
+        {
+            get { return (string)GetValue(SearchTermProperty); }
+            set { SetValue(SearchTermProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for SearchTerm.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty SearchTermProperty =
+            DependencyProperty.Register("SearchTerm", typeof(string), typeof(SearchControl), new PropertyMetadata(""));
+
+        public delegate void OnSearchClick(object sender, SearchEventArgs e);
+        public event OnSearchClick SearchButtonClick;
+
+        public override void OnApplyTemplate()
+        {
+            base.OnApplyTemplate();
+            if (GetTemplateChild("PART_Button") is Button button)
+            {
+                button.Click += Button_Click;
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            //throw new NotImplementedException();
+            SearchButtonClick?.Invoke(this, new SearchEventArgs { SearchTerm = SearchTerm });
+        }
+    }
+}
---
+++ L053/L053/SearchEventArgs.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L053
+{
+    public class SearchEventArgs: EventArgs
+    {
+        public string SearchTerm { set; get; }
+    }
+}
