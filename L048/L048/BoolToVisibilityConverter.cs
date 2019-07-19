9988b7be84178e8cf1f3da1e7da63b91223d3022
zmrbak <20995658@qq.com>
2019-07-20 06:55:07 +0800

    L048：如何在WPF中使用值转换器

---
+++ L048/L048/BoolToVisibilityConverter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+
+namespace L048
+{
+    public class BoolToVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            //throw new NotImplementedException();
+            //return value is bool val && val ? Visibility.Visible : Visibility.Collapsed;
+            var val = (bool)value;
+            if (parameter is string param && param == "inverse") val = !val;
+
+            return val ? Visibility.Visible : Visibility.Collapsed;
+
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
