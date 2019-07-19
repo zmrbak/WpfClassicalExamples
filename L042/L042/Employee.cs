399c9ecf672a2e3c2b237ae68f3d3a3b41ff7208
zmrbak <20995658@qq.com>
2019-07-20 06:51:06 +0800

    L042：如何在WPF中把数据绑定到集合

---
+++ L042/L042/Employee.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L042
+{
+    public class Employee
+    {
+        public string FirstName { set; get; }
+        public string LastName { set; get; }
+        public string Department { set; get; }
+    }
+}
---
+++ L042/L042/MainWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace L042
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Employees = new ObservableCollection<Employee>
+            {
+                new Employee{ FirstName="FirstName_1", LastName="LastName_1", Department="Department_1" },
+                new Employee{ FirstName="FirstName_2", LastName="LastName_2", Department="Department_2" },
+                new Employee{ FirstName="FirstName_3", LastName="LastName_3", Department="Department_3" },
+                new Employee{ FirstName="FirstName_4", LastName="LastName_4", Department="Department_4" },
+                new Employee{ FirstName="FirstName_5", LastName="LastName_5", Department="Department_5" },
+                new Employee{ FirstName="FirstName_6", LastName="LastName_6", Department="Department_6" },
+                new Employee{ FirstName="FirstName_7", LastName="LastName_7", Department="Department_7" },
+                new Employee{ FirstName="FirstName_8", LastName="LastName_8", Department="Department_8" }
+            };
+            dataGrid.ItemsSource = Employees;
+        }
+
+
+
+        public ObservableCollection<Employee> Employees
+        {
+            get { return (ObservableCollection<Employee>)GetValue(EmployeesProperty); }
+            set { SetValue(EmployeesProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for Employees.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty EmployeesProperty =
+            DependencyProperty.Register("Employees", typeof(ObservableCollection<Employee>), typeof(MainWindow), new PropertyMetadata(null));
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            ;
+        }
+    }
+}
