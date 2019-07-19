56430493687c361bbb9e5d47e1eb2611687de38b
zmrbak <20995658@qq.com>
2019-07-20 06:50:28 +0800

    L041：如何在WPF中把数据绑定到对象

---
+++ L041/L041/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace L041
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            PersonDetails = new Person { Blog = "http://www.baidu.com", Name = "赵庆明", Experience = 10 };
+
+            DataContext = PersonDetails;
+        }
+
+
+        public Person PersonDetails
+        {
+            get { return (Person)GetValue(PersonDetailsProperty); }
+            set { SetValue(PersonDetailsProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for PersonDetails.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty PersonDetailsProperty =
+            DependencyProperty.Register("PersonDetails", typeof(Person), typeof(MainWindow), new PropertyMetadata(null));
+
+
+    }
+}
---
+++ L041/L041/Person.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L041
+{
+    public class Person
+    {
+        public string Name { set; get; }
+        public string Blog { set; get; }
+        public int Experience { set; get; }
+    }
+}
