ae60faa71fccd54dcf51d7ae3303f8cf32116236
zmrbak <20995658@qq.com>
2019-07-30 22:22:15 +0800

    L068：如何在WPF的隐藏代码中访问二进制资源

---
+++ L068/L068/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace L068
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            //var steamResourceInfo = Application.GetResourceStream(new Uri("Images/imag1.jpg",UriKind.RelativeOrAbsolute));
+            var steamResourceInfo = Application.GetResourceStream(new Uri("/ClassLibrary1;component/imag4.jpg", UriKind.RelativeOrAbsolute));
+
+            var bitmapImage = new BitmapImage();
+            bitmapImage.BeginInit();
+            bitmapImage.CacheOption = BitmapCacheOption.OnLoad;
+            bitmapImage.StreamSource = steamResourceInfo.Stream;
+            bitmapImage.EndInit();
+            bitmapImage.Freeze();
+
+            imageTest.Source = bitmapImage;
+        }
+    }
+}
