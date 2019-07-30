923a506a24802b34be5bcb833211281c1d999956
zmrbak <20995658@qq.com>
2019-07-30 22:09:20 +0800

    L055：如何在WPF中创建和使用用户控件

---
+++ L055/L055/ColorMixer.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace L055
+{
+    /// <summary>
+    /// ColorMixer.xaml 的交互逻辑
+    /// </summary>
+    public partial class ColorMixer : UserControl
+    {
+        public ColorMixer()
+        {
+            InitializeComponent();
+        }
+
+
+        public Color SelectedColor
+        {
+            get { return (Color)GetValue(SelectedColorProperty); }
+            set { SetValue(SelectedColorProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for SelectedColor.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty SelectedColorProperty =
+            DependencyProperty.Register("SelectedColor", typeof(Color), typeof(ColorMixer), new PropertyMetadata(Colors.DarkBlue));
+
+
+    }
+}
