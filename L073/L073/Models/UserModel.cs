c7fce468e25bac60bb33852eb39de52aaed08649
zmrbak <20995658@qq.com>
2019-07-30 22:26:08 +0800

    L073：如何使用MVVM模式来构建WPF应用程序

---
+++ L073/L073/Models/UserModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L073.Models
+{
+    public class UserModel
+    {
+        public String FirstName { set; get; }
+        public String LastName { set; get; }
+    }
+}
---
+++ L073/L073/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,56 @@
+﻿using L073.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L073.ViewModels
+{
+    public class MainWindowViewModel : INotifyPropertyChanged
+    {
+        public MainWindowViewModel()
+        {
+            UserCollection = new ObservableCollection<UserModel>
+            {
+                new UserModel {FirstName="User",LastName="1"},
+                new UserModel {FirstName="User",LastName="2"},
+                new UserModel {FirstName="User",LastName="3"},
+                new UserModel {FirstName="User",LastName="4"},
+                new UserModel {FirstName="User",LastName="5"},
+                new UserModel {FirstName="User",LastName="6"}
+            };
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged(String propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        private UserModel m_selectedUser;
+        public UserModel SelectedUser
+        {
+            get { return m_selectedUser; }
+            set
+            {
+                m_selectedUser = value;
+                OnPropertyChanged("SelectedUser");
+            }
+        }
+
+        private ObservableCollection<UserModel> m_userCollection;
+        public ObservableCollection<UserModel> UserCollection
+        {
+            get { return m_userCollection; }
+            set
+            {
+                m_userCollection = value;
+                OnPropertyChanged("UserCollection");
+            }
+        }
+
+    }
+}
