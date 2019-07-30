c3f64aff1a81c9f1df33aad5bb7ad6a23cc6e25d
zmrbak <20995658@qq.com>
2019-07-30 22:35:18 +0800

    L085：如何创建一个自托管的WCF服务

---
+++ L085/L085/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.ServiceModel.Description;
+using System.Text;
+using System.Threading.Tasks;
+using WcfService1.Services;
+
+namespace L085
+{
+    class Program
+    {
+        private static Uri serviceUri = new Uri("http://localhost:51841/Services/EmployeeService.svc");
+        static void Main(string[] args)
+        {
+            using (var serviceHost = new ServiceHost(typeof(EmployeeService), serviceUri))
+            {
+                serviceHost.AddServiceEndpoint(typeof(IEmployeeService), new BasicHttpBinding(), "");
+                serviceHost.Description.Behaviors.Add(
+                    new ServiceMetadataBehavior
+                    {
+                        HttpGetEnabled = true
+                    }
+                );
+
+                serviceHost.Open();
+                Console.WriteLine("当前时间:" + DateTime.Now.ToString()) ;
+                Console.WriteLine("服务执行中...");
+                Console.WriteLine("按回车停止服");
+                Console.ReadLine();
+                serviceHost.Close();
+            }
+        }
+    }
+}
---
+++ L085/WcfService1/Services/IEmployeeService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using WcfService1.DataModels;
+
+namespace WcfService1.Services
+{
+    // 注意: 使用“重构”菜单上的“重命名”命令，可以同时更改代码和配置文件中的接口名“IEmployeeService”。
+    [ServiceContract]
+    public interface IEmployeeService
+    {
+        //[OperationContract]
+        //void DoWork();
+        [OperationContract]
+        Employee GetEmployeeByID(String employeeID);
+
+        [OperationContract]
+        List<Employee> GetEmployees();
+
+        [OperationContract]
+        void InsertEmployee(Employee employee);
+    }
+}
