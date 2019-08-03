12c7b8d596e0a62eae81d9722ea7340d97b631c2
zmrbak <20995658@qq.com>
2019-08-03 20:52:38 +0800

    添加代码

---
+++ 配套代码/L100/L100/App.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace L100
+{
+    /// <summary>
+    /// App.xaml 的交互逻辑
+    /// </summary>
+    public partial class App : Application
+    {
+        private static Assembly OnResolveAssembly(object sender, ResolveEventArgs args)
+        {
+            Assembly executingAssembly = Assembly.GetExecutingAssembly();
+            var executingAssemblyName = executingAssembly.GetName();
+            var resName = executingAssemblyName.Name + ".resources";
+
+            AssemblyName assemblyName = new AssemblyName(args.Name); string path = "";
+            if (resName == assemblyName.Name)
+            {
+                path = executingAssemblyName.Name + ".g.resources"; ;
+            }
+            else
+            {
+                path = assemblyName.Name + ".dll";
+                if (assemblyName.CultureInfo.Equals(CultureInfo.InvariantCulture) == false)
+                {
+                    path = String.Format(@"{0}\{1}", assemblyName.CultureInfo, path);
+                }
+            }
+
+            using (Stream stream = executingAssembly.GetManifestResourceStream(path))
+            {
+                if (stream == null)
+                    return null;
+
+                byte[] assemblyRawBytes = new byte[stream.Length];
+                stream.Read(assemblyRawBytes, 0, assemblyRawBytes.Length);
+                return Assembly.Load(assemblyRawBytes);
+            }
+        }
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            AppDomain.CurrentDomain.AssemblyResolve += OnResolveAssembly;
+        }
+    }
+}
